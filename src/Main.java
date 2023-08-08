b8b963f500b9faae17ba610baa592a89fa3caa7b
Artem <milkartem2@mail.ru>
2023-08-08 14:11:07 +0300

    added hangman

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Vector;
+
+public class Hangman {
+
+    public static void main(String[] args) {
+        UI.loop();
+    }
+}
